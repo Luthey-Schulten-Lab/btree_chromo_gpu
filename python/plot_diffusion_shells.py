6240054d11a99032089e0d9271e497a30f7500a4
Ben Gilbert <brg4@illinois.edu>
2022-11-10 17:22:37 -0600

    updated DNA model format

--- python/plot_diffusion_shells.py
+++ python/plot_diffusion_shells.py
@@ -19,19 +19,19 @@ import diffusion_plotting as d_plot
 imp.reload(d_plot)
 
 in_dir = '/home/ben/Data/btree_chromo/diffusion_testing/'
-in_label = 'diffusion_bond_bending_twisting'
+in_label = 'diffusion_no_interactions'
 min_rep = 1
-max_rep = 20
+max_rep = 10
 
 out_dir = '/home/ben/Documents/svn/Minimal_Cell_Chromosome_Organization_2022/tex_figures/diffusion/raw_plots/'
-out_label = 'diffusion_bond_bending_twisting'
+out_label = 'diffusion_no_interactions'
 
 timestep_options = ['Last','FirstLast','Slices','All']
 timestep_select = timestep_options[3]
 slices = [0,50000,100000,150000,200000,250000]
 
-write_shell_diff = True
-read_shell_diff = False
+write_shell_diff = False
+read_shell_diff = True
 
 if not os.path.isdir(out_dir) and write_shell_diff:
     os.makedirs(out_dir)
