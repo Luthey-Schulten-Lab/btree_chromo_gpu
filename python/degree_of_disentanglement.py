51b5018c7cb74bc76c50472cc5d40db3b2f36960
Ben Gilbert <brg4@illinois.edu>
2023-01-26 16:32:06 -0600

    rearranged files and made progress on disentanglement calculations

--- python/degree_of_disentanglement.py
+++ python/degree_of_disentanglement.py
@@ -22,100 +22,46 @@ import segregation_calculations.disentanglement_plotting as de_p
 importlib.reload(de_p)
 
 in_dir = '/home/ben/Data/btree_chromo/partition_testing/'
-in_label = 'partition_test'
-f_p_label = 'test_f_p'
+in_label = 'partition_noloops_test'
+f_p_label = 'test_noloops_f_p'
 min_rep = 1
-max_rep = 3
-
-t_d = np.arange(500000,2500000+1,50000,dtype=np.int32)
-R = 4.0*17.0
-t_d = np.sort(t_d)
-print(t_d)
+max_rep = 2
 
 out_dir = '/home/ben/Data/btree_chromo/partition_testing/'
 out_label = 'partition_test'
 
+write_DoD = True
+read_DoD = False
+
 if not os.path.isdir(out_dir):
     os.makedirs(out_dir)
 
-rep_count = 0
+DoD_file = out_dir + out_label + '_DoD.pickle'
 
-for rep in range(min_rep,max_rep+1):
+ts_DoD = np.arange(500000,2500000+1,50000,dtype=np.int32)
+R = 4.0*17.0
+ts_DoD = np.sort(ts_DoD)
 
-    # read the trajectory
+if write_DoD:
 
-    rep_label = '_rep' + str(rep).zfill(5)
+    rep_count = 0
 
-    in_file = in_dir + in_label + rep_label + '.pkl_traj'
+    for rep in range(min_rep,max_rep+1):
 
-    print('LAMMPS trajectory: ' + in_file)
+        # read the trajectory
 
-    traj = r_L_D.read_pickle_traj(in_file)
+        rep_label = '_rep' + str(rep).zfill(5)
 
-    # get the set of fork partition files
+        in_file = in_dir + in_label + rep_label + '.pkl_traj'
 
-    f_p_files = in_dir + f_p_label + rep_label + '_*.dat'
+        print('LAMMPS trajectory: ' + in_file)
 
-    f_p_files = glob.glob(f_p_files)
+        traj = r_L_D.read_pickle_traj(in_file)
 
-    ts_f_ps = dict()
+        # get the set of fork partition files
 
-    ts_f_ps['ts'] = np.zeros((len(f_p_files)),dtype=np.int32)
-    ts_f_ps['fork_partitions'] = []
-
-    for i in range(len(f_p_files)):
-
-        t = f_p_files[i].split('_')
-        t = t[-1]
-        t = t.split('.')
-        t = t[0]
-        t = t.strip('t')
-
-        ts_f_ps['ts'][i] = int(t)
-
-        f_ps = f_p.fork_partitions()
-        ts_f_ps['fork_partitions'].append(f_ps)
-
-    ts_arg_sorted = np.argsort(ts_f_ps['ts'])
-
-    for i_t in range(ts_arg_sorted.shape[0]):
-
-        ts_f_ps['fork_partitions'][i_t].parse_f_p_file(f_p_files[ts_arg_sorted[i_t]])
-
-    ts_f_ps['ts'] = np.sort(ts_f_ps['ts'])
-
-    print(ts_f_ps['ts'])
-    print(ts_f_ps['fork_partitions'])
-    
-    for i_t in range(t_d.shape[0]):
-
-        i_t_m = t_d.shape[0] - 1 - i_t
-        
-        ts_f_p_selected = np.argwhere(ts_f_ps['ts']<t_d[i_t_m])[-1][0]
-
-        f_ps = ts_f_ps['fork_partitions'][ts_f_p_selected]
-
-        x = traj[str(t_d[i_t_m])]['DNA']['x']
-        print(str(t_d[i_t_m]))
-        print(x.shape[0])
-        
-        forks, d_o_d = de_p.disentanglement(f_ps,x,R)
-        
-        if i_t == 0:
-            final_forks = forks
-            d_o_d_forks_ts = np.zeros((len(final_forks),t_d.shape[0]),dtype=np.double)
-
-        for i_fork in range(len(forks)):
-
-            for j_fork in range(len(final_forks)):
-
-                if forks[i_fork] == final_forks[j_fork]:
-
-                    d_o_d_forks_ts[j_fork,i_t_m] = d_o_d[i_fork]
-
-    print(d_o_d_forks_ts)
-
-        
+        f_p_files = in_dir + f_p_label + rep_label + '_*.dat'
+      
 
         
             
--- python/segregation_calculations/disentanglement_plotting.py
+++ python/segregation_calculations/disentanglement_plotting.py
@@ -11,6 +11,7 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable
 import os
 
 import pickle
+import glob
 
 
 # import matplotlib.font_manager as font_manager
@@ -27,6 +28,133 @@ plt.rcParams['font.sans-serif'] = 'Arial'
 
 mm = 1/25.4
 l_w_ratio = 1/1.618
+
+def new_DoD(R,N_reps,N_t,t):
+
+    DoD = dict()
+
+    DoD['R'] = R
+    DoD['N_reps'] = N_reps
+    DoD['N_t'] = N_t
+    DoD['t'] = t
+
+    DoD['forks'] = []
+    DoD['N_forks'] = 0
+
+    return DoD
+
+def fill_reps_DoD(DoD,traj,f_p_files,i_rep):
+
+    # use glob to get all of the fork_partition files
+    f_p_files = glob.glob(f_p_files)
+
+    # create a dictionary of the timesteps with new fork_partitions
+    ts_f_ps = dict()
+    ts_f_ps['ts'] = np.zeros((len(f_p_files)),dtype=np.int32)
+    ts_f_ps['fork_partitions'] = []
+
+    for i in range(len(f_p_files)):
+
+        t = f_p_files[i].split('_')
+        t = t[-1]
+        t = t.split('.')
+        t = t[0]
+        t = t.strip('t')
+
+        ts_f_ps['ts'][i] = int(t)
+
+        f_ps = f_p.fork_partitions()
+        # append an empty fork_partition
+        ts_f_ps['fork_partitions'].append(f_ps)
+
+    # find args to sort the timesteps of the fork_partitions
+    ts_arg_sorted = np.argsort(ts_f_ps['ts'])
+
+    # loop over the sorted args and parse the fork_partition files
+    for i_t in range(ts_arg_sorted.shape[0]):
+        ts_f_ps['fork_partitions'][i_t].parse_f_p_file(f_p_files[ts_arg_sorted[i_t]])
+
+    # now sort the timetseps
+    ts_f_ps['ts'] = np.sort(ts_f_ps['ts'])
+
+    # loop over the timesteps for the disentangle calculations
+    for i_t in range(DoD['N_t']):
+
+        # increment backwards to begin with the most replicated state
+        i_t_m = DoD['N_t'] - 1 - i_t
+
+        # select fork_partition from most recent update
+        ts_f_p_selected = np.argwhere(ts_f_ps['ts']<DoD['t'][i_t_m])[-1][0]
+        f_ps = ts_f_ps['fork_partitions'][ts_f_p_selected]
+
+        # get the coordinates of the DNA at the timestep
+        x = traj[str(DoD['t'][i_t_m])]['DNA']['x']
+
+        # get the forks and calculate the DoD
+        forks, d = disentanglement(f_ps,x,R)
+
+        # perform actions for first iteration
+        if i_t == 0:
+            # store the final set of forks
+            final_forks = forks
+            # allocate array with results
+            d_forks_ts = np.zeros((len(final_forks),DoD['N_t']),
+                                  dtype=np.double)
+
+        for i_fork in range(len(forks)):
+
+            for j_fork in range(len(final_forks)):
+
+                if forks[i_fork] == final_forks[j_fork]:
+
+                    d_forks_ts[j_fork,i_t_m] = d_o_d[i_fork]
+
+    if i_rep == 0:
+
+        DoD['N_forks'] = len(final_forks)
+        DoD['forks'] = final_forks
+
+        DoD['d_reps_forks_ts'] = np.zeros((DoD['N_reps'],
+                                           DoD['N_forks'],
+                                           DoD['N_t']),
+                                          dtype=np.double)
+
+        DoD['d_reps_forks_ts'][0,:,:] = d_forks_ts
+
+    else:
+
+        for i_fork in range(len(final_forks)):
+
+            k = -1
+
+            for j_fork in range(DoD['N_forks']):
+
+                if final_forks[i_fork] == DoD['forks'][j_fork]:
+
+                    k = j_fork
+                    break
+
+            if k != -1:
+
+                DoD['d_reps_forks_ts'][i_rep,k,:] = d_forks_ts[i_fork,:]
+
+            else:
+
+                DoD['N_forks'] += 1
+                DoD['forks'].append(final_forks[i_fork])
+
+                # add new elements to array
+                zero_d_forks_ts = np.zeros((DoD['N_reps'],
+                                            DoD['N_t']),
+                                           dtype=np.double)
+
+                np.append(DoD['d_reps_forks_ts'],
+                          zero_d_forks_ts,
+                          axis=1)
+
+                DoD['d_reps_forks_ts'][i_rep,DoD['N_forks']-1,:] = d_forks_ts[i_fork,:]
+    
+    return DoD
 
 def disentanglement(f_ps,x,R):
 
@@ -51,7 +179,7 @@ def disentanglement(f_ps,x,R):
         # print(a_l.shape)
         # print(a_r.shape)
 
-        phi_bar_l, phi_bar_r = step_proximity_fraction(x,
+        phi_bar_l, phi_bar_r = tanh_proximity_fraction(x,
                                                        N_l,
                                                        N_r,
                                                        a_l,
@@ -73,7 +201,7 @@ def step_proximity_fraction(x,N_l,N_r,a_l,a_r,R2):
 
     n_o_l, n_o_r = step_proximity_opposite(x,N_l,a_l,N_r,a_r,R2)
 
-    alpha = 1.0 #N_l/N_r
+    alpha = N_l/N_r
     phi_l = np.divide(n_s_l,(n_s_l + alpha*n_o_l))
     phi_r = np.divide(n_s_r,(n_s_r + n_o_r/alpha))
 
@@ -128,5 +256,81 @@ def step_proximity_opposite(x,N_l,a_l,N_r,a_r,R2):
 
         n_o_r[p] += 1
         n_o_l[i] += p.shape[0]
+    
+    return n_o_l, n_o_r
+
+def tanh_proximity_fraction(x,N_l,N_r,a_l,a_r,R2):
+
+    phi_bar_l = 0.0
+    phi_bar_r = 0.0
+
+    n_s_l = tanh_proximity_same(x,N_l,a_l,R2)
+    n_s_r = tanh_proximity_same(x,N_r,a_r,R2)
+
+    n_o_l, n_o_r = tanh_proximity_opposite(x,N_l,a_l,N_r,a_r,R2)
+
+    alpha = N_l/N_r
+    phi_l = np.divide(n_s_l,(n_s_l + alpha*n_o_l))
+    phi_r = np.divide(n_s_r,(n_s_r + n_o_r/alpha))
+
+    phi_bar_l = np.mean(phi_l)
+    phi_bar_r = np.mean(phi_r)
+
+    # print("phi_bar_l = " + str(phi_bar_l))
+    # print("phi_bar_r = " + str(phi_bar_r))
+    
+    return phi_bar_l, phi_bar_r
+
+def tanh_proximity_same(x,N,a,R2):
+
+    R2i = 1.0/R2
+
+    xt = x[a,:]
+
+    n_s = np.zeros((N),dtype=np.double)
+
+    for i in range(N-1):
+        
+        xc = xt[i,:]
+        xnc = xt[i+1:,:]
+
+        dx = xnc - np.tile(xc,(xnc.shape[0],1))
+
+        d2 = np.sum(np.power(dx,2.0),axis=1)
+
+        y = np.sqrt(R2i*d2)
+        y = 1.0 + np.tanh(-y)
+        
+        p = np.arange(i+1,N,1,dtype=np.int32)
+
+        n_s[p] += y
+        n_s[i] += np.sum(y)
+
+    return n_s
+
+def tanh_proximity_opposite(x,N_l,a_l,N_r,a_r,R2):
+
+    R2i = 1.0/R2
+
+    n_o_l = np.zeros((N_l),dtype=np.double)
+    n_o_r = np.zeros((N_r),dtype=np.double)
+
+    x_l = x[a_l,:]
+    x_r = x[a_r,:]
+
+    for i in range(N_l):
+
+        xc = x_l[i,:]
+        xnc = x_r
+
+        dx = xnc - np.tile(xc,(xnc.shape[0],1))
+
+        d2 = np.sum(np.power(dx,2.0),axis=1)
+
+        y = np.sqrt(R2i*d2)
+        y = 1.0 + np.tanh(-y)
+
+        n_o_r += y
+        n_o_l[i] += np.sum(y)
     
     return n_o_l, n_o_r
